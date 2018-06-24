d0fff8bc19eaec73df396b0c512f87a1ce5d1c98
lxykad <503215875@qq.com>
2018-06-24 22:36:19 +0800

    add image

--- app/src/main/java/com/lxy/test/ui/FlexActivity.java
+++ app/src/main/java/com/lxy/test/ui/FlexActivity.java
@@ -12,6 +12,9 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.blankj.utilcode.util.ConvertUtils;
+import com.blankj.utilcode.util.ScreenUtils;
+import com.blankj.utilcode.util.Utils;
 import com.bumptech.glide.Glide;
 import com.google.android.flexbox.FlexboxLayout;
 import com.lxy.test.R;
@@ -23,11 +26,23 @@ import com.lxy.test.entity.FlexBean;
 public class FlexActivity extends AppCompatActivity {
 
     private FlexboxLayout flexboxLayout;
+    private int screenWidth;
+    private static final int COUNT = 4; // 每行图片的个数
+    private int imgWidth;// 每个图片的宽度
+    private int imgHeight;
+    private int distance;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_flex);
+
+        screenWidth = ScreenUtils.getScreenWidth();
+        int totalWidth = screenWidth - ConvertUtils.dp2px((COUNT + 1) * 10);
+        imgWidth = (int) (totalWidth * 1f / COUNT);
+        imgHeight = imgWidth;
+
+        distance = ConvertUtils.dp2px(10);
 
         flexboxLayout = findViewById(R.id.flex_layout);
         FlexBean bean = new FlexBean();
@@ -47,10 +62,12 @@ public class FlexActivity extends AppCompatActivity {
             ImageView imageView = (ImageView) LayoutInflater.from(this)
                     .inflate(R.layout.item_img_layout, null);
 
-            ViewGroup.LayoutParams params = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+            FlexboxLayout.LayoutParams params = new FlexboxLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
 
-            params.height = 240;
-            params.width = 240;
+            params.height = imgHeight;
+            params.width = imgWidth;
+            params.leftMargin = distance;
+            params.topMargin = distance;
 
 
             imageView.setLayoutParams(params);
@@ -58,6 +75,15 @@ public class FlexActivity extends AppCompatActivity {
             Glide.with(this)
                     .load(bean.imgUrl)
                     .into(imageView);
+
+            final int finalI = i;
+            imageView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+                    Toast.makeText(view.getContext(), finalI + "", Toast.LENGTH_SHORT).show();
+                }
+            });
 
             flexboxLayout.addView(imageView);
 
